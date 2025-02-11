741e40807c7eac96a22eb2d4fec84d66072c0e1a
umaxcode <maxwellodoom1729@gmail.com>
2025-02-11 09:22:05 +0000

    chore: install and configure ShadCN for UI styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <div className="bg-red-300 text-white text-center">Hello world</div>
+      <Button>Click me</Button>
     </>
   );
 }
