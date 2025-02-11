c827995fc71eda9540fc2471a6b3ed7ffc3c34e2
umaxcode <maxwellodoom1729@gmail.com>
2025-02-11 12:00:02 +0000

    feat: add application routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { Button } from "./components/ui/button";
+import {
+  createBrowserRouter,
+  createRoutesFromChildren,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import AuthPage from "./pages/auth/AuthPage";
+import { LoginCallBack } from "./pages/auth/LoginCallBack";
+import HomePageLayout from "./pages/home/HomePageLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromChildren(
+    <Route path="/">
+      <Route index element={<AuthPage />} />
+      <Route path="auth/callback" element={<LoginCallBack />} />
+      <Route path="home" element={<HomePageLayout />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <>
-      <div className="bg-red-300 text-white text-center">Hello world</div>
-      <Button>Click me</Button>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
