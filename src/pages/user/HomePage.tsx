51d2b600b583a2b042e8ca0bef6951935f26ffa6
umaxcode <maxwellodoom1729@gmail.com>
2025-02-24 16:12:54 +0000

    feat: update HomPage with functionality to share and delete images

--- src/pages/user/HomePage.tsx
+++ src/pages/user/HomePage.tsx
@@ -6,9 +6,23 @@ import { Progress } from "../../components/ui/progress";
 import { Card, CardContent } from "../../components/ui/card";
 import { Skeleton } from "../../components/ui/skeleton";
 import { useAuth } from "../../contexts/AuthContext";
-import { fetchUsersPost } from "../../services/UserServiceHttp";
+import {
+  fetchUsersPost,
+  sharePost,
+  movePostToRecyclingBin,
+} from "../../services/UserServiceHttp";
 import { Endpoints } from "../../backend/endpoints";
 import { api } from "../../backend/api";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "../../components/ui/dialog";
+import { Textarea } from "../../components/ui/textarea";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
 
 type PhotoType = {
   imgId: string;
@@ -24,6 +38,11 @@ const HomePage = () => {
   const [photos, setPhotos] = useState<PhotoType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isSharingCardId, setIsSharingCardId] = useState<string>();
+  const [isDeletingCardId, setIsDeletingCardId] = useState<string>();
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const [preSignedUrl, setPreSignedUrl] = useState<string>("");
 
   useEffect(() => {
     const socket = new WebSocket(`${Endpoints.WSS}?email=${email}`);
@@ -57,6 +76,37 @@ const HomePage = () => {
       console.error("Error fetching photos:", error);
     } finally {
       setIsLoading(false);
+    }
+  };
+
+  const sharePhoto = async (id: string) => {
+    setIsSharingCardId(id);
+    try {
+      const results = await sharePost(id);
+      console.log(results);
+      setPreSignedUrl(results.data.picUrl);
+      setIsDialogOpen(true);
+    } catch (error) {
+      console.error("Error fetching photos:", error);
+    } finally {
+      setIsSharingCardId("");
+    }
+  };
+
+  const deletePhoto = async (id: string) => {
+    setIsDeletingCardId(id);
+    try {
+      const results = await movePostToRecyclingBin(id);
+      const removedPhotoId = results.data.imgId;
+      const updatedPost = photos.filter(
+        (photo) => photo.imgId !== removedPhotoId
+      );
+      console.log(removedPhotoId);
+      setPhotos(updatedPost);
+    } catch (error) {
+      console.error("Error fetching photos:", error);
+    } finally {
+      setIsDeletingCardId("");
     }
   };
 
@@ -99,100 +149,142 @@ const HomePage = () => {
     }
   };
 
+  const handleCopy = () => {
+    if (textAreaRef.current) {
+      textAreaRef.current.select();
+      navigator.clipboard.writeText(textAreaRef.current.value);
+    }
+  };
+
   return (
-    <div className="p-4">
-      {/* Welcome Section */}
-      <section className="bg-orange-500 rounded-sm p-3 text-center text-white">
-        <h1 className="text-[1.5rem] font-bold">
-          Welcome back {firstName} {lastName} !!
-        </h1>
-        <p className="mt-2">This is the Home Page of the Photo Blog App.</p>
-      </section>
+    <>
+      <div className="p-4">
+        {/* Welcome Section */}
+        <section className="bg-orange-500 rounded-sm p-3 text-center text-white">
+          <h1 className="text-[1.5rem] font-bold">
+            Welcome back {firstName} {lastName} !!
+          </h1>
+          <p className="mt-2">This is the Home Page of the Photo Blog App.</p>
+        </section>
 
-      {/* Upload Section */}
-      <section className="mt-8 max-w-[75rem] mx-auto">
-        <div>
-          <h2 className="text-[1.1rem] font-bold text-center text-gray-600 mb-3">
-            Upload Image for Processing
-          </h2>
-          <div className="max-w-md mx-auto p-4 border rounded-lg shadow">
-            <Label
-              htmlFor="file-upload"
-              className="mb-2 block text-sm font-medium text-gray-600"
-            >
-              Upload a Photo
-            </Label>
-            <Input
-              type="file"
-              id="file-upload"
-              ref={fileInputRef}
-              onChange={handleFileChange}
-            />
-            {uploading && <Progress value={uploadProgress} className="mt-2" />}
-            <Button
-              onClick={handleUpload}
-              className="mt-4 w-full bg-orange-600 hover:bg-orange-300"
-              disabled={!pic || uploading}
-            >
-              {uploading ? "Uploading..." : "Upload"}
-            </Button>
+        {/* Upload Section */}
+        <section className="mt-8 max-w-[75rem] mx-auto">
+          <div>
+            <h2 className="text-[1.1rem] font-bold text-center text-gray-600 mb-3">
+              Upload Image for Processing
+            </h2>
+            <div className="max-w-md mx-auto p-4 border rounded-lg shadow">
+              <Label
+                htmlFor="file-upload"
+                className="mb-2 block text-sm font-medium text-gray-600"
+              >
+                Upload a Photo
+              </Label>
+              <Input
+                type="file"
+                id="file-upload"
+                ref={fileInputRef}
+                onChange={handleFileChange}
+              />
+              {uploading && (
+                <Progress value={uploadProgress} className="mt-2" />
+              )}
+              <Button
+                onClick={handleUpload}
+                className="mt-4 w-full bg-orange-600 hover:bg-orange-300"
+                disabled={!pic || uploading}
+              >
+                {uploading ? "Uploading..." : "Upload"}
+              </Button>
+            </div>
           </div>
-        </div>
 
-        {/* Images Section */}
-        <div className="mt-6">
-          <h2 className="text-[1.1rem] font-bold text-gray-600 mb-2">
-            All User's Posts
-          </h2>
+          {/* Images Section */}
+          <div className="mt-6">
+            <h2 className="text-[1.1rem] font-bold text-gray-600 mb-2">
+              All User's Posts
+            </h2>
 
-          {/* Loading Skeleton */}
-          {isLoading ? (
-            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4">
-              {[...Array(4)].map((_, index) => (
-                <Card
-                  key={index}
-                  className="rounded-lg overflow-hidden shadow-md"
-                >
-                  <CardContent className="p-0">
-                    <Skeleton className="w-full h-60" />
-                  </CardContent>
-                </Card>
-              ))}
-            </div>
-          ) : photos.length > 0 ? (
-            <div className="mt-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {photos.map((photo, index) => (
-                <Card
-                  key={index}
-                  className="rounded-lg overflow-hidden shadow-md cursor-pointer"
-                >
-                  <CardContent className="p-0">
-                    <img
-                      src={`data:image/png;base64,${photo.image}`}
-                      alt={`Uploaded ${index}`}
-                      className="w-full h-60 object-cover"
-                    />
-                  </CardContent>
-                  <div className="flex justify-between items-center p-1">
-                    <Button className="bg-orange-600/90 hover:bg-orange-600">
-                      Share
-                    </Button>
-                    <Button className="bg-red-600/90 hover:bg-red-600">
-                      Delete
-                    </Button>
-                  </div>
-                </Card>
-              ))}
-            </div>
-          ) : (
-            // No images message
-            <div className="text-center text-gray-500 mt-6">
-              No images available
-            </div>
-          )}
-        </div>
-      </section>
-    </div>
+            {/* Loading Skeleton */}
+            {isLoading ? (
+              <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4">
+                {[...Array(4)].map((_, index) => (
+                  <Card
+                    key={index}
+                    className="rounded-lg overflow-hidden shadow-md"
+                  >
+                    <CardContent className="p-0">
+                      <Skeleton className="w-full h-60" />
+                    </CardContent>
+                  </Card>
+                ))}
+              </div>
+            ) : photos.length > 0 ? (
+              <div className="mt-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {photos.map((photo, index) => (
+                  <Card
+                    key={index}
+                    className="rounded-lg overflow-hidden shadow-md cursor-pointer"
+                  >
+                    <CardContent className="p-0">
+                      <img
+                        src={`data:image/png;base64,${photo.image}`}
+                        alt={`Uploaded ${index}`}
+                        className="w-full h-60 object-cover"
+                      />
+                    </CardContent>
+                    <div className="flex justify-between items-center p-1">
+                      <Button
+                        className="bg-orange-600/90 hover:bg-orange-600"
+                        onClick={() => sharePhoto(photo.imgId)}
+                      >
+                        {isSharingCardId == photo.imgId ? "Sharing" : "Share"}
+                      </Button>
+                      <Button
+                        className="bg-red-600/90 hover:bg-red-600"
+                        onClick={() => deletePhoto(photo.imgId)}
+                      >
+                        {isDeletingCardId == photo.imgId
+                          ? "Deleting"
+                          : "Delete"}
+                      </Button>
+                    </div>
+                  </Card>
+                ))}
+              </div>
+            ) : (
+              // No images message
+              <div className="text-center text-gray-500 mt-6">
+                No images available
+              </div>
+            )}
+          </div>
+        </section>
+      </div>
+      <Dialog open={isDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogPrimitive.Close
+              className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+              onClick={() => setIsDialogOpen(false)}
+            >
+              <X className="h-4 w-4" />
+              <span className="sr-only">Close</span>
+            </DialogPrimitive.Close>
+            <DialogTitle>Copy and send pre-signed url</DialogTitle>
+            <DialogDescription>This url expires in 3 hours</DialogDescription>
+
+            <Textarea value={preSignedUrl} ref={textAreaRef} />
+            <Button
+              className="mt-2 bg-orange-600/90 hover:bg-orange-600"
+              onClick={() => handleCopy()}
+            >
+              Copy url
+            </Button>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
