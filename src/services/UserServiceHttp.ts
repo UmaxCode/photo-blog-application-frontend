8e63ce11ddfbfcb352b963b8633fccf653fb512b
umaxcode <maxwellodoom1729@gmail.com>
2025-02-24 16:10:39 +0000

    feat: update service with methods to make calls to the backend

--- src/services/UserServiceHttp.ts
+++ src/services/UserServiceHttp.ts
@@ -10,3 +10,67 @@ export const fetchUsersPost = async () => {
     throw error;
   }
 };
+
+export const fetchOthersPost = async () => {
+  try {
+    const response = await api.get(`${Endpoints.PHOTOS}/others-photo`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const movePostToRecyclingBin = async (id: string) => {
+  try {
+    const response = await api.patch(`${Endpoints.PHOTOS}/${id}/recycle-bin`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const sharePost = async (id: string) => {
+  try {
+    const response = await api.get(
+      `${Endpoints.PHOTOS}/${id}/generate-pre-signed-url`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const fetchUserRecycledPost = async () => {
+  try {
+    const response = await api.get(`${Endpoints.PHOTOS}/recycle-bin`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const restorePostFromRecyclingBin = async (id: string) => {
+  try {
+    const response = await api.patch(
+      `${Endpoints.PHOTOS}/${id}/recycle-bin/restore`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const permanentlyDeletePost = async (id: string) => {
+  try {
+    const response = await api.delete(`${Endpoints.PHOTOS}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
